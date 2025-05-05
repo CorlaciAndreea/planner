5d0ce5d9040fee566ee54e69a5d0623cb763d019
Corlaci Andreea <corlaciandreea99@gmail.com>
2025-05-05 19:17:41 +0300

    Add final set of tests for planning

--- src/test/java/com/github/corlaciandreea/planner/TestPlanningController.java
+++ src/test/java/com/github/corlaciandreea/planner/TestPlanningController.java
@@ -11,6 +11,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -24,12 +25,28 @@ public class TestPlanningController {
 
     @Test
     public void testAddingWrongPlanning() throws Exception {
+        // The wishbook entries do not exist for employees
         this.mockMvc.perform(put("/planning/2025-05-02/early")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content("[\"employee12\", \"employee13\"]"))
                 .andExpect(status().isBadRequest())
                 .andExpect(result -> assertTrue(result.getResolvedException().getMessage().contains("Wish book entry does not exists for ")));
-        ;
+
+        addWishBookEntriesForEmployees("early");
+        // Only one employee was added to the plan
+        this.mockMvc.perform(put("/planning/2025-05-02/early")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("[\"employee121\"]"))
+                .andExpect(status().isBadRequest())
+                .andExpect(result -> assertTrue(result.getResolvedException().getMessage().contains("There must be two employees per shift.")));
+
+        // More than 2 employees were added to the plan
+        this.mockMvc.perform(put("/planning/2025-05-02/early")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("[\"employee121\", \"employee131\", \"employee141\"]"))
+                .andExpect(status().isBadRequest())
+                .andExpect(result -> assertTrue(result.getResolvedException().getMessage().contains("Only two employees per shift are allowed.")));
+        addWishBookEntriesForEmployees("late");
     }
 
     @Test
@@ -46,6 +63,42 @@ public class TestPlanningController {
         this.mockMvc.perform(put("/planning/2025-05-02/early")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content("[\"employee100\", \"employee101\"]"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testValidSchedule() throws Exception {
+        // Add employees wises to validate the plan
+        // Add employees wises to validate the plan
+        this.mockMvc.perform(put("/wish/employee100/2025-05-02")
+                .contentType(MediaType.TEXT_PLAIN)
+                .content("early"));
+        this.mockMvc.perform(put("/wish/employee101/2025-05-02")
+                .contentType(MediaType.TEXT_PLAIN)
+                .content("early"));
+        this.mockMvc.perform(put("/wish/employee102/2025-05-02")
+                .contentType(MediaType.TEXT_PLAIN)
+                .content("early"));
+        this.mockMvc.perform(put("/wish/employee103/2025-05-02")
+                .contentType(MediaType.TEXT_PLAIN)
+                .content("early"));
+
+        this.mockMvc.perform(get("/schedule/2025-05-02"))
+                .andExpect(status().isOk());
+    }
+
+    private void addWishBookEntriesForEmployees(String shift) throws Exception {
+        this.mockMvc.perform(put("/wish/employee121/2025-05-02")
+                        .contentType(MediaType.TEXT_PLAIN)
+                        .content(shift))
+                .andExpect(status().isOk());
+        this.mockMvc.perform(put("/wish/employee131/2025-05-02")
+                        .contentType(MediaType.TEXT_PLAIN)
+                        .content(shift))
+                .andExpect(status().isOk());
+        this.mockMvc.perform(put("/wish/employee141/2025-05-02")
+                        .contentType(MediaType.TEXT_PLAIN)
+                        .content(shift))
                 .andExpect(status().isOk());
     }
 
