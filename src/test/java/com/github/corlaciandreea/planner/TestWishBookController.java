1fb0aaa5410c442b0a6de6f09f281a29e67462c0
Corlaci Andreea <corlaciandreea99@gmail.com>
2025-05-05 17:51:44 +0300

    Add test for WishBookController

--- src/test/java/com/github/corlaciandreea/planner/TestWishBookController.java
+++ src/test/java/com/github/corlaciandreea/planner/TestWishBookController.java
@@ -9,6 +9,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -22,9 +23,19 @@ public class TestWishBookController {
 
     @Test
     public void testAddingAWish() throws Exception {
+
+        // Add a good employee wish
         this.mockMvc.perform(put("/wish/employee1/2025-05-02")
                         .contentType(MediaType.TEXT_PLAIN)
                         .content("early"))
                 .andExpect(status().isOk());
+
+        // Add a wrong shift name and check the
+        this.mockMvc.perform(put("/wish/employee1/2025-05-02")
+                        .contentType(MediaType.TEXT_PLAIN)
+                        .content("later"))
+                .andExpect(status().isBadRequest())
+                .andExpect(result -> assertTrue(result.getResolvedException().getMessage().contains("The shift is not valid.")));
+
     }
 }
