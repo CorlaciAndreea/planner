0e0fb2eba2838fe8d24b5a9ef07aaf307e142f0c
Corlaci Andreea <corlaciandreea99@gmail.com>
2025-05-05 21:25:19 +0300

    Add API documentation using swagger

--- src/main/java/com/github/corlaciandreea/planner/controller/PlanningController.java
+++ src/main/java/com/github/corlaciandreea/planner/controller/PlanningController.java
@@ -4,6 +4,12 @@ import com.github.corlaciandreea.planner.error.ValidationException;
 import com.github.corlaciandreea.planner.model.DaySchedule;
 import com.github.corlaciandreea.planner.model.Plan;
 import com.github.corlaciandreea.planner.service.PlanningService;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,10 +26,29 @@ public class PlanningController {
     @Autowired
     private PlanningService planningService;
 
+    @Tag(name = "addPlanning", description = "Add wish book entries to schedule")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "The planning was saved for a given date and shift",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Plan.class))}),
+            @ApiResponse(responseCode = "400", description = "The shift is not valid.",
+                    content = @Content),
+            @ApiResponse(responseCode = "400", description = "There must be two employees per shift.",
+                    content = @Content),
+            @ApiResponse(responseCode = "400", description = "Only two employees per shift are allowed.",
+                    content = @Content),
+            @ApiResponse(responseCode = "400", description = "Wish book entry does not exists for some employees.",
+                    content = @Content),
+            @ApiResponse(responseCode = "400", description = "The employee already has one shift planned for the given day.",
+                    content = @Content)
+    })
     @PutMapping("/planning/{date}/{shift}")
     public Plan addPlanning(@PathVariable("date") LocalDate date,
                             @PathVariable("shift") String shift,
-                            @RequestBody List<String> emplyeesList) {
+                            @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                                    description = "The employee list for the planning", required = true,
+                                    content = @Content(mediaType = "application/json",
+                                            examples = @ExampleObject(value = "[\"employee1\", \"employee2\"]"))) @RequestBody List<String> emplyeesList) {
         try {
             Plan newPlanning = new Plan(date, shift, emplyeesList);
             return this.planningService.savePlanPerShift(newPlanning);
@@ -32,6 +57,15 @@ public class PlanningController {
         }
     }
 
+    @Tag(name = "seeSchedule", description = "Get the schedule for a given date")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "The schedule for the given day is returned",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = DaySchedule.class),
+                            examples = @ExampleObject(value = "{\"date\":\"2025-05-02\",\"shifts\":{\"early\":[\"employee6\",\"employee1\"],\"late\":[\"employee3\",\"employee5\",\"employee4\"]}}"))}),
+            @ApiResponse(responseCode = "204", description = "There is no schedule for that specific date.",
+                    content = @Content)
+    })
     @GetMapping("/schedule/{date}")
     public DaySchedule getScheduleForDay(@PathVariable("date") LocalDate date) {
         return this.planningService.getSchedulePerDay(date);
--- src/main/java/com/github/corlaciandreea/planner/controller/WishBookController.java
+++ src/main/java/com/github/corlaciandreea/planner/controller/WishBookController.java
@@ -3,6 +3,12 @@ package com.github.corlaciandreea.planner.controller;
 import com.github.corlaciandreea.planner.error.ValidationException;
 import com.github.corlaciandreea.planner.model.WishBookEntry;
 import com.github.corlaciandreea.planner.service.WishBookService;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
@@ -16,10 +22,21 @@ public class WishBookController {
     @Autowired
     private WishBookService wishBookService;
 
+    @Tag(name = "addWishBookEntry", description = "Add the desired shift per date")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "The employee wish was saved",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = WishBookEntry.class))}),
+            @ApiResponse(responseCode = "400", description = "The shift is not valid.",
+                    content = @Content)})
     @PutMapping("/{employeeId}/{date}")
     public WishBookEntry saveWishBookEntry(@PathVariable("employeeId") String employeeId,
                                            @PathVariable("date") LocalDate date,
-                                           @RequestBody String shift) {
+                                           @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                                                   description = "Desired shift", required = true,
+                                                   content = @Content(mediaType = "application/json",
+                                                           schema = @Schema(implementation = WishBookEntry.class),
+                                                           examples = {@ExampleObject(value = "early"), @ExampleObject(value = "late")})) @RequestBody String shift) {
         try {
             WishBookEntry newEntry = new WishBookEntry(employeeId, date, shift);
             return this.wishBookService.saveWishBookEntry(newEntry);
